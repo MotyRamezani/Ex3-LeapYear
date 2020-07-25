256c600ac2c678063c46c792d1a46b165c3e655e
M.Ramezani <68474204+MotyRamezani@users.noreply.github.com>
2020-07-25 20:27:01 +0430

    Update Ex7 - Fibonacci.py

--- Ex7 - Fibonacci.py
+++ Ex7 - Fibonacci.py
@@ -1,7 +1,9 @@
 def fib(arg) :
     if arg <= 0 :
         print("Please choose a number grater than 0.")
-    elif arg == 1 or arg == 2 :
+    elif arg == 1 :
+        return 0
+    elif arg == 2 :   
         return 1
-    elif arg > 2 :
+    else :
         return fib(arg-1) + fib(arg-2)
